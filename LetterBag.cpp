34b8476f59ec24973758aba94412defdbabd1dc1
Nicole <nwu12@student.ohlone.edu>
2025-05-12 16:48:56 -0700

    Update LetterBag.cpp

--- LetterBag.cpp
+++ LetterBag.cpp
@@ -8,7 +8,31 @@ LetterBag::LetterBag() {
     // Format: addTiles(letter, count, point_value)
     addTiles('A', 9, 1);
     addTiles('B', 2, 3);
-    // ... (rest of initialization)
+    addTiles('C', 2, 3);
+    addTiles('D', 4, 2);
+    addTiles('E', 12, 1);
+    addTiles('F', 2, 4);
+    addTiles('G', 3, 2);
+    addTiles('H', 2, 4);
+    addTiles('I', 9, 1);
+    addTiles('J', 1, 8);
+    addTiles('K', 1, 5);
+    addTiles('L', 4, 1);
+    addTiles('M', 2, 3);
+    addTiles('N', 6, 1);
+    addTiles('O', 8, 1);
+    addTiles('P', 2, 3);
+    addTiles('Q', 1, 10);
+    addTiles('R', 6, 1);
+    addTiles('S', 4, 1);
+    addTiles('T', 6, 1);
+    addTiles('U', 4, 1);
+    addTiles('V', 2, 4);
+    addTiles('W', 2, 4);
+    addTiles('X', 1, 8);
+    addTiles('Y', 2, 4);
+    addTiles('Z', 1, 10);
+    addTiles('*', 2, 0); // Blank tiles
 }
 
 // Adds specified number of tiles to the bag
